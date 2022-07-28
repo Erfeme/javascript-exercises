182f4d8493d02c4111adb19150ea17d7b43f4e75
enod <win_1118@hotmail.com>
2022-07-28 10:19:20 -0500

    Fibonacci and palindromes finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    fiboArray=[1,1];
+
+    if(n>0){
+
+        for (i=1; i<n;i++){
+
+            nextFibo = fiboArray[i] + fiboArray[i-1];
+            
+            fiboArray.push(nextFibo);
+    
+        }
+    
+        return fiboArray[n-1];
+        
+    } else {
+
+        return "OOPS"
+    }
+
+    
 
 };
 
