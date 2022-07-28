3f8a4426fe9564c84aeeb69e62cd5e443f3dc357
enod <win_1118@hotmail.com>
2022-07-28 12:35:34 -0500

    Completed findTheOldest, regular methods, need to practice map and reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {    
+    
+    let oldest ={};
+    let oldestAge = 0;
 
+    people.forEach((person)=>{
+        
+        if (person.hasOwnProperty('yearOfDeath')){
+
+            personAge = person.yearOfDeath - person.yearOfBirth;
+
+            if (personAge > oldestAge){
+                oldestAge = personAge;
+                oldest = person;
+            }
+
+        } else {
+
+            personAge = new Date().getFullYear() - person.yearOfBirth ;
+
+            if (personAge > oldestAge){
+
+                oldestAge = personAge;
+
+                oldest = person;
+            }
+        }
+        
+    })
+
+    return oldest;
 };
 
 // Do not edit below this line
